5cd2e71724da341cc1e33b658d2a26a4913065de
Kamil Bednarski <kamilarturbednarski@gmail.com>
2021-07-08 17:44:25 +0200

    update: comment interface

---
+++ src/app/comment.ts
@@ -0,0 +1,18 @@
+import { User } from "./user";
+
+export interface Comment {
+    id: number,
+    user: User,
+    content: string,
+    numberOfLikes: number,
+    numberOfShares: number,
+    dateOfCreation: string,
+    timeOfCreation: string,
+    wasItModified: boolean,
+    dateOfModification: string,
+    timeOfModification: string,
+    ageOfCommentInYears: number,
+    ageOfCommentInDays: number,
+    ageOfCommentInHours: number,
+    ageOfCommentInMinutes: number
+}
