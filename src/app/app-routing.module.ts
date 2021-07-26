c7b625adafac30fcca27e0af9bfe3b399cd363e3
Kamil Bednarski <kamilarturbednarski@gmail.com>
2021-07-26 21:09:37 +0200

    update: adjusted routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { PostDetailsComponent } from './post-details/post-details.component';
 import { PostListComponent } from './post-list/post-list.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
   { path: '', component: PostListComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistrationComponent },
   { path: 'post/:postId', component: PostDetailsComponent }
 ];
 
